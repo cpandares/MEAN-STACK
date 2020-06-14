dde8b9edc75efc03f7d2235bf187de8e56077a15
unknown <pandaresocesar@gmail.com>
2020-06-14 10:29:25 -0400

    subiendo app

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ var app = require('./app');
 var port = 3700;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/portafolio')
+//Conexion en local
+//mongoose.connect('mongodb://localhost:27017/portafolio')
+
+//conexion en la nube
+mongoose.connect('mongodb+srv://user:8*QDYs63qDtL=a6@portafolio-i5phl.mongodb.net/portafolio?retryWrites=true&w=majority')
         .then(() => {
         	console.log("Conexión a la base de datos establecida satisfactoriamente...");
 
